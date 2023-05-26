27df542c26016c3b4255d5d8c3e7d862636a87a5
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-26 10:38:31 -0700

    colors are slightly better and the head is a unique color from rest of body

--- index.py
+++ index.py
@@ -9,7 +9,8 @@ class snake(object):
         self.length = 1
         self.positions = [((screenWidth / 2), (screenHeight / 2))]
         self.direction = random.choice([up, down, left, right])
-        self.color = (150, 24, 47)
+        self.color = (115, 199, 199)
+
 
     def getHeadPosition(self):
         return self.positions[0]
@@ -26,12 +27,13 @@ class snake(object):
         new = (((cur[0] + (x*gridSize)) % screenWidth), ((cur[1] + (y*gridSize)) % screenHeight))
         if len(self.positions) > 2 and new in self.positions[2:]:
             self.reset()
+            
         else:
             self.positions.insert(0, new)   
             if len(self.positions) > self.length:
                 self.positions.pop()
-        if x > gridSize or y > gridSize:
-            self.reset()
+        # if cur > gridSize or cur > gridSize:
+        #     self.reset()
 
     def handleKeys(self):
         for event in pygame.event.get():
@@ -53,15 +55,17 @@ class snake(object):
         self.length = 1
         self.positions = [((screenWidth / 2), (screenHeight / 2))]
         self.direction = random.choice([up, down, left, right])
-        # score = 0
 
 
-
-    def draw(self, surface):
+    def draw(self, surface):        
         for p in self.positions:
             r = pygame.Rect((p[0], p[1]), (gridSize, gridSize))
-            pygame.draw.rect(surface, self.color, r)
-            pygame.draw.rect(surface, (93, 216, 228), r, 1)
+            if p == self.positions[0]:
+                pygame.draw.rect(surface, (105, 180, 180), r)
+            else:
+                pygame.draw.rect(surface, self.color, r)
+
+            # pygame.draw.rect(surface, (93, 216, 228), r, 1)
 
 
 class food(object):
@@ -69,13 +73,15 @@ class food(object):
         self.position = (0,0)
         self.color = (223, 163, 49)
         self.randomizePosition()
+        self.color = (150, 24, 47)
+
 
     def randomizePosition(self):
         self.position = (random.randint(0, gridWidth-1) * gridSize, random.randint(0, gridHeight-1) * gridSize)
     def draw(self, surface):
         r = pygame.Rect((self.position[0], self.position[1]), (gridSize, gridSize))
         pygame.draw.rect(surface, self.color, r)
-        pygame.draw.rect(surface, (93, 216, 228), r, 1)
+        # pygame.draw.rect(surface, (93, 216, 228), r, 1)
 
 
 
@@ -84,20 +90,21 @@ def drawGrid(surface):
         for x in range(0, int(gridWidth)):
             if (x + y) % 2 == 0:
                 r = pygame.Rect((x*gridSize, y*gridSize), (gridSize, gridSize))
-                pygame.draw.rect(surface, (96, 216, 228), r)
+                pygame.draw.rect(surface, (52, 212, 37), r)
+                
             else:
                 rr = pygame.Rect((x*gridSize, y*gridSize), (gridSize, gridSize))
-                pygame.draw.rect(surface, (85, 194, 205), rr)
+                pygame.draw.rect(surface, (60, 240, 50), rr)
 
 
-screenWidth = 480
-screenHeight = 480
-gridSize = 20
+screenWidth = 576
+screenHeight = 576
+gridSize = 24
 gridWidth = screenWidth / gridSize
 gridHeight = screenHeight / gridSize
 
 
-
+baseScore = 0
 
 up = (0, -1)
 down = (0, 1)
@@ -114,8 +121,8 @@ def main():
     surface = surface.convert()
     drawGrid(surface)
 
-    score = 0
-
+    
+    score = baseScore
 
     snakeReference = snake()
     foodReference = food()
@@ -133,12 +140,16 @@ def main():
             snakeReference.length += 1
             score += 1
             foodReference.randomizePosition()
+        
         #Handle Events
         snakeReference.draw(surface)
         foodReference.draw(surface)
         screen.blit(surface, (0,0))
         text = myFont.render("Score {0}".format(score), 1, (0, 0, 0))
         screen.blit(text, (5, 10))
+        # if len(snakeReference.positions) < 1:
+        #     score = 0
         pygame.display.update()
+        
 
 main()
